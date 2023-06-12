cbc2a40489df08af84617ae953105b650bf0ff2f
MadhavShroff <madhavshroff99@gmail.com>
2023-06-12 11:57:27 -0400

    Style Changes, Added a component, no functional changes or additions.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const {fontFamily} = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,4 +9,15 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [],
+  theme: {
+    extend: {
+      colors: { 
+        'slate': '##101010'
+      },
+      fontFamily: {
+        'primary': ['var(--sora-font)', ...fontFamily.sans],
+        'serif': ['var(--sora-font)', ...fontFamily.serif]
+      },
+    }
+  }
 }
