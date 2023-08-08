cb561184512098cbe645cc2c703a168934e71e63
MadhavShroff <madhavshroff99@gmail.com>
2023-08-08 00:35:45 -0400

    Fixing logout

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,3 @@
 export const cognitoHostedUI = `https://api.makeitaifor.me/auth/cognito`;
 export const cookieName = "makeitaifor-me-jwt-cookie";
-export const cognitoLogoutUrl = 'https://makeitaifor-me.auth.us-east-2.amazoncognito.com/logout?client_id=1fu7ekpv47p0rbctrfagaimnjt&logout_uri=https://api.makeitaifor.me/logout-redirect/';
+export const cognitoLogoutUrl = 'https://makeitaifor-me.auth.us-east-2.amazoncognito.com/logout?client_id=1fu7ekpv47p0rbctrfagaimnjt&logout_uri=https://api.makeitaifor.me/auth/cognito/logout-redirect/';
