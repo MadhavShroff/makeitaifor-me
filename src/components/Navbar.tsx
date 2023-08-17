2ce2147dd3244d8eb004a1ce88a6b871f0cd69e3
MadhavShroff <madhavshroff99@gmail.com>
2023-08-17 15:15:20 -0400

    Made Navbar Responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,71 +28,62 @@ const Navbar: React.FC<NavbarProps> = (props) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <nav
-      className={`flex items-center justify-between bg-white shadow-lg px-1`}
-    >
+    <nav className={`flex items-center justify-between bg-white px-1 flex-wrap rounded`}>
       <Link
         href="/"
-        className="px-2 py-1 sm:py-2 sm:text-xl text-2xl font-bold text-black hover:underline decoration-orange-500 decoration-4"
-      >
+        className="px-2 py-1 sm:py-2 text-2xl font-bold text-black hover:underline decoration-orange-500">
         MakeIt<span className="text-orange-500">Ai</span>For.
         <span className="text-orange-500">Me</span>
       </Link>
-      <div className="relative w-full h-full overflow-hidden hor-scroll-wrap justify-end">
-        <div
-          id="navbar"
-          className="flex gap-2 sm:gap-2 overflow-x-auto justify-end scrollbar-hide hor-scroll pl-5"
-        >
-          {links.map((link, index) => (
-            <Link
-              key={index}
-              href={link.href}
-              className="px-3 sm:px-4 py-1 sm:py-1 text-lg sm:text-xl border rounded-full hover:bg-orange-500 whitespace-nowrap font-bold"
-              style={{
-                borderColor: 'var(--background-color)',
-                color: 'var(--background-color)',
-              }}
-            >
-              {link.text}
-            </Link>
-          ))}
-          {props.user != null ? (
-            <Link
-              key={11}
-              href={'/profile'}
-              className="flex items-center pr-1 pt-1 pb-1 pl-3 text-lg sm:text-xl border rounded-full hover:bg-orange-500 whitespace-nowrap font-bold"
-              style={{
-                borderColor: 'var(--background-color)',
-                color: 'var(--background-color)',
-              }}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <span className="pr-2">
-                {isHovered ? "View Profile" : 'Hi ' + props.user.name.split(" ")[0] + '!'}
-              </span>
-              <div className="h-7 w-7 rounded-full object-cover border-black border-2">
-                <img
-                  src={`https://source.boringavatars.com/marble/100/${props.user.id}?colors=EF233C,FED4E7,313638,003E1F`}
-                  alt="Profile Picture"
-                />
-              </div>
-            </Link>
-          ) : (
-            <Link
-              key={12}
-              href={'/auth'} // replace with your logout link
-              className="px-3 py-1 text-lg sm:text-xl border rounded-full hover:bg-orange-500 whitespace-nowrap text-center font-bold"
-              style={{
-                borderColor: 'var(--background-color)',
-                color: 'var(--background-color)',
-              }}
-            >
-              {'Login'}
-            </Link>
-          )}
-        </div>
-        {isScrollable && <div className="navbar-fade"></div>}
+      <div className='flex gap-1 flex-wrap my-1'>
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className="px-3 sm:px-4 py-1 sm:py-1 text-lg sm:text-xl border rounded-full hover:bg-orange-500 whitespace-nowrap font-bold sm:h-8"
+            style={{
+              borderColor: 'var(--background-color)',
+              color: 'var(--background-color)',
+            }}
+          >
+            {link.text}
+          </Link>
+        ))}
+        {props.user != null ? (
+          <Link
+            key={11}
+            href={'/profile'}
+            className="flex items-center pr-1 pt-1 pb-1 pl-3 text-lg sm:text-xl border rounded-full hover:bg-orange-500 whitespace-nowrap font-bold mx-1"
+            style={{
+              borderColor: 'var(--background-color)',
+              color: 'var(--background-color)',
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <span className="pr-2">
+              {isHovered ? "View Profile" : 'Hi ' + props.user.name.split(" ")[0] + '!'}
+            </span>
+            <div className="h-7 w-7 rounded-full object-cover border-black border-2">
+              <img
+                src={`https://source.boringavatars.com/marble/100/${props.user.id}?colors=EF233C,FED4E7,313638,003E1F`}
+                alt="Profile Picture"
+              />
+            </div>
+          </Link>
+        ) : (
+          <Link
+            key={12}
+            href={'/auth'} // replace with your logout link
+            className="px-3 py-1 text-lg sm:text-xl border rounded-full hover:bg-orange-500 whitespace-nowrap text-center font-bold"
+            style={{
+              borderColor: 'var(--background-color)',
+              color: 'var(--background-color)',
+            }}
+          >
+            {'Login'}
+          </Link>
+        )}
       </div>
     </nav>
   );
