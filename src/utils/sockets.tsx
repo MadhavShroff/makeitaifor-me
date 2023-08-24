0097a6e42d4d7bb7a2ca2935ab8cfdfb733393d6
MadhavShroff <madhavshroff99@gmail.com>
2023-08-24 16:52:32 -0400

    debug

--- src/utils/sockets.tsx
+++ src/utils/sockets.tsx
@@ -36,24 +36,24 @@ async function getWebSocketToken() {
 
 let socket: Socket;
 
-(async () => {
-  
-  console
-  const token = await getWebSocketToken();
-  socket = io(`wss://api.makeitaifor.me?token=${token}`);
+// (async () => {
 
-  socket.on('connect', () => {
-    console.log('Connected to WebSocket');
-  })
+//   console
+//   const token = await getWebSocketToken();
+//   socket = io(`wss://api.makeitaifor.me?token=${token}`);
 
-  socket.on('error', (error) => {
-    console.error('Error:', error);
-  });
+//   socket.on('connect', () => {
+//     console.log('Connected to WebSocket');
+//   })
 
-  socket.on('message', (message) => {
-    console.log('Received message from server: ', message);
-  });
-})()
+//   socket.on('error', (error) => {
+//     console.error('Error:', error);
+//   });
+
+//   socket.on('message', (message) => {
+//     console.log('Received message from server: ', message);
+//   });
+// })()
 
 
 export const emitTryButtonClicked = (
