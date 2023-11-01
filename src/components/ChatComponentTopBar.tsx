a11d77c305e6dcc196c7a55322a3fbef5e7d863a
MadhavShroff <madhavshroff99@gmail.com>
2023-11-01 13:29:44 -0400

    sets selected chat or file/collection in the ChatComponentTopBar

--- src/components/ChatComponentTopBar.tsx
+++ src/components/ChatComponentTopBar.tsx
@@ -13,6 +13,8 @@ export const ChatComponentTopBar = (props: ChatComponentTopBarPropType) => {
     const [modelsAvailable, setModelsAvailable] = useState<Model[]>([]);
     const [fileNamesArr, setFileNamesArr] = useState<{ name: string, fileKey: string }[]>([]);
     const [filesData, setFilesData] = useState<FileData[]>([]);
+    const [selectedFile, setSelectedFile] = useState<FileData | null>(null);
+    const [selectedModel, setSelectedModel] = useState<Model | null>(null);
 
     const context = React.useContext(ChatContext);
     if (!context) throw new Error("YourChildComponent must be used within a ChatProvider");
@@ -57,12 +59,13 @@ export const ChatComponentTopBar = (props: ChatComponentTopBarPropType) => {
                         <button
                             type="button"
                             onClick={() => { setCollectionsDropdownVisible(!isCollectionsDropdownVisible); setModelDropdownVisible(false); }}
-                            className="inline-flex w-full justify-center items-center text-left gap-x-1.5 rounded-md bg-black px-3 py-0.5 text-sm sm:text-xs font-semibold text-white hover:text-black shadow-sm hover:bg-orange-500"
+                            className={"inline-flex w-full justify-center items-center text-left gap-x-1.5 rounded-md px-3 py-0.5 text-sm sm:text-xs font-semibold hover:text-black shadow-sm hover:bg-orange-500 " + (selectedFile == null ? "bg-black text-white" : "bg-white text-black")}
                             id="menu-button"
                             aria-expanded={isCollectionsDropdownVisible ? 'true' : 'false'}
                             aria-haspopup="true"
                         >
-                            Use File or Collection
+                            {selectedFile == null && "Use File or Collection"}
+                            {selectedFile != null && selectedFile.meta.Key.split('/')[1]}
                             <svg className="-mr-1 h-5 w-5 text-gray-400 group-hover:text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                 <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                             </svg>
@@ -79,7 +82,12 @@ export const ChatComponentTopBar = (props: ChatComponentTopBarPropType) => {
                                     </a>
                                 }
                                 {fileNamesArr.map((file, index) => (
-                                    <a key={index} href="#" className="flex items-center px-2 py-1 hover:text-black hover:bg-white rounded mx-1" role="menuitem">
+                                    <a key={index} href="#" className="flex items-center px-2 py-1 hover:text-black hover:bg-white rounded mx-1" role="menuitem" onClick={
+                                        () => {
+                                            setSelectedFile(filesData[index]);
+                                            setCollectionsDropdownVisible(false);
+                                        }
+                                    }>
                                         <svg className='mr-2 h-10 w-10 flex-shrink-0' width="40" height="40" viewBox="0 0 120 80" xmlns="http://www.w3.org/2000/svg">
                                             <rect x="40" y="0" width="80" height="50" fill="#8B5CF6" rx="5" ry="5" stroke="white" stroke-width="1" />
                                             <rect x="32" y="6" width="80" height="50" fill="#000000" rx="5" ry="5" stroke="white" stroke-width="1" />
@@ -101,12 +109,14 @@ export const ChatComponentTopBar = (props: ChatComponentTopBarPropType) => {
                         <button
                             type="button"
                             onClick={onModelDropdownClicked}
-                            className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-black px-3 py-0.5 text-sm sm:text-xs font-semibold text-white hover:text-black shadow-sm hover:bg-orange-500"
+                            className={
+                                "inline-flex w-full justify-center items-center gap-x-1.5 rounded-md px-3 py-0.5 text-sm sm:text-xs font-semibold hover:text-black shadow-sm hover:bg-orange-500 " + (selectedModel == null ? "bg-black text-white" : "bg-white text-black") }
                             id="menu-button"
                             aria-expanded={isModelDropdownVisible ? 'true' : 'false'}
                             aria-haspopup="true"
                         >
-                            Model
+                            {selectedModel == null && "Select Model"}
+                            {selectedModel != null && "Using " + selectedModel.name}
                             <svg className="-mr-1 h-5 w-5 text-gray-400 group-hover:text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                 <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                             </svg>
@@ -116,7 +126,11 @@ export const ChatComponentTopBar = (props: ChatComponentTopBarPropType) => {
                         <div className="absolute right-0 z-10 mt-1 w-40 origin-top-right rounded-md bg-black shadow-lg ring-1 ring-white focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
                             <div className="py-1" role="none">
                                 {modelsAvailable.map((model, index) => (
-                                    <a key={index} href="#" className="flex items-center px-4 py-2 hover:text-black hover:bg-white rounded mx-1" role="menuitem">
+                                    <a key={index} href="#" className="flex items-center px-4 py-2 hover:text-black hover:bg-white rounded mx-1" role="menuitem"
+                                    onClick={() => {
+                                        setSelectedModel(model);
+                                        setModelDropdownVisible(false);
+                                    }}>
                                         {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="mr-2 h-5 w-5">
                                             <path d="M7 3.5A1.5 1.5 0 018.5 2h3.879a1.5 1.5 0 011.06.44l3.122 3.12A1.5 1.5 0 0117 6.622V12.5a1.5 1.5 0 01-1.5 1.5h-1v-3.379a3 3 0 00-.879-2.121L10.5 5.379A3 3 0 008.379 4.5H7v-1z"></path>
                                             <path d="M4.5 6A1.5 1.5 0 003 7.5v9A1.5 1.5 0 004.5 18h7a1.5 1.5 0 001.5-1.5v-5.879a1.5 1.5 0 00-.44-1.06L9.44 6.439A1.5 1.5 0 008.378 6H4.5z"></path>
