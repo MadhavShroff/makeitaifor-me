0aa18026d4288b0e5b87303d7410ce373c6102fa
Madhav Shroff <madhavshroff99@gmail.com>
2024-05-16 16:47:10 -0400

    Revert "Added mdx dependencies"

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-import createMDX from '@next/mdx'
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  reactStrictMode: true
 }
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
