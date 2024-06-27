4c7c45d8b30a8bd14ff03bd3fb62efebb8f572d1
Madhav Shroff <madhavshroff99@gmail.com>
2024-06-27 08:40:51 -0400

    Update index.tsx, temporarily remove ModuleSection from homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const Home = () => {
       <Image src="/image16.svg" alt="image" className="z-0 absolute w-[24vh] right-[-5vh] top-[110vh] xl:top-[70vh] 2xl:top-[105vh] 2xl:right-[5vh] rotate-45 text-orange-500 w-[20vh] " width={500} height={500}/>
       <Image src="/image17.svg" alt="image" className="z-0 absolute w-[24vh] left-[-5vh] top-[70vh] xl:top-[110vh] 2xl:top-[70vh] 2xl:left-[5vh] rotate-45 text-orange-500 w-[20vh] " width={500} height={500}/>
       <ProductSection />
-      <ModuleSection />
+{/*       <ModuleSection /> */}
       <Footer />
     </main>
   );
